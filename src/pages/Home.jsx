67e6673aa9687008bb55d0f6893a76ea6789326d
unknown <fernandobriceno9999@gmail.com>
2025-12-02 17:17:02 -0500

    added images folder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function Home() {
   return (
-    <div className="bg-white dark:bg-gray-900 min-h-screen flex items-center justify-center">
+    <div className="bg-indigo-600 min-h-screen flex items-center justify-center">
       <h1>This is the Home Page Test</h1>
     </div>
   );
