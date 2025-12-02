50e47f223837a5d56a6f39f7288e6280174ba7f7
unknown <fernandobriceno9999@gmail.com>
2025-12-02 17:13:22 -0500

    update home test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,8 @@ import { useState } from "react";
 
 function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      
+    <div className="bg-white dark:bg-gray-900 min-h-screen flex items-center justify-center">
+      <h1>This is the Home Page Test</h1>
     </div>
   );
 }
