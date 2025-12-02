b464c3cbcaa0e66c32de9a34756234bc2bad53f3
unknown <fernandobriceno9999@gmail.com>
2025-12-02 16:56:44 -0500

    update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,10 @@
 import { useState } from "react";
 
 function Home() {
-  const [message, setMessage] = useState("Fernando");
-
-  function changeMessage() {
-    setMessage("HI FERNANDO");
-  }
-
   return (
     <div>
       <h1>Home</h1>
-      <p>{message}</p>
-      <button onClick={changeMessage}>CLICK FERNANDO</button>
+      
     </div>
   );
 }
