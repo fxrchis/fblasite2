2d20b6779e3106d978891c0aac30abf5309b35bc
Eric Fernandes <furiousemerald5@gmail.com>
2025-12-02 17:15:02 -0500

    UpdatedSubmission

--- src/pages/Submission.jsx
+++ src/pages/Submission.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 
 function Submission() {
-
+  const [title, setTitle] = useState("");
   return (
     <div>
       <input type="text" id="item-submisson" required name="item-submission"></input>
+
     </div>
   )
 }
