182c7fe283cda15e49aab9744e031c32e7e40ccf
Eric Fernandes <furiousemerald5@gmail.com>
2025-12-04 16:20:44 -0500

    updatedSearch

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,12 @@
 import { useState } from 'react'
-
+import { supabase } from "./supabaseClient";
 function Search() {
-
+  //variables
+  const [searchTerm, setSearchTerm] = useState("");
+  const [category, setCategory] = useState("all");
+  const [items, setItems] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
   return (
     <h1>Search</h1>
   )
