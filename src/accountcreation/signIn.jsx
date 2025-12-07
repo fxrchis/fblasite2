d14b7e26e89fe6353267574683afdd45cbd589bd
unknown <fernandobriceno9999@gmail.com>
2025-12-07 13:20:12 -0500

    submission, account creation

--- src/accountcreation/signIn.jsx
+++ src/accountcreation/signIn.jsx
@@ -3,8 +3,42 @@ import { useState } from 'react'
 function SignIn() {
 
   return (
-    <div>
-      <h1>hi test</h1>
+    <div className='w-full bg-blue-100 h-screen p-4 flex flex-col items-center justify-center font-outfit'>
+      <div className='bg-gray-50 shadow-xl w-1/3 min-h-fit rounded-xl flex flex-col p-6 gap-6 border-blue-500 border-2'>
+
+        <h1 className='text-3xl font-bold text-center'>Sign In</h1>
+
+        <div>
+          Need an account? <a href="/signup" className='text-blue-500 hover:underline'>Sign Up</a>
+        </div>
+
+        <div className='w-full flex flex-col gap-4'>
+          <input
+          type="text"
+          placeholder="Username"
+          className='p-3 border border-gray-300 rounded-lg focus:outline-none'
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className='p-3 border border-gray-300 rounded-lg focus:outline-none'
+          />
+        </div>
+        
+        <div className='flex'>
+          <input
+          type="checkbox"
+          />
+          <label className='ml-2'>Remember Me</label>
+        </div>
+
+        <div className='mt-auto'>
+          <button className='w-1/2 bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-300 flex justify-center mx-auto'>
+            Sign In
+          </button>
+        </div>
+        
+      </div>
     </div>
   )
 }
--- src/accountcreation/signUp.jsx
+++ src/accountcreation/signUp.jsx
@@ -3,7 +3,43 @@ import { useState } from 'react'
 function SignUp() {
 
   return (
-    <h1>signUp</h1>
+    <div className='w-full bg-blue-100 h-screen p-4 flex flex-col items-center justify-center font-outfit'>
+      <div className='bg-gray-50 shadow-xl w-1/3 min-h-fit rounded-xl flex flex-col p-6 gap-6 border-blue-500 border-2'>
+
+        <h1 className='text-3xl font-bold text-center'>Sign Up</h1>
+
+        <div>
+          Already have an account? <a href="/signin" className='text-blue-500 hover:underline'>Sign In</a>
+        </div>
+
+        <div className='w-full flex flex-col gap-4'>
+          <input
+          type="text"
+          placeholder="Username"
+          className='p-3 border border-gray-300 rounded-lg focus:outline-none'
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className='p-3 border border-gray-300 rounded-lg focus:outline-none'
+          />
+        </div>
+        
+        <div className='flex'>
+          <input
+          type="checkbox"
+          />
+          <label className='ml-2'>Remember Me</label>
+        </div>
+
+        <div className='mt-auto'>
+          <button className='w-1/2 bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-300 flex justify-center mx-auto'>
+            Create Account
+          </button>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
