e48ed76bdb74829dedcb22f3de2b936dcca0a9d7
Eric Fernandes <furiousemerald5@gmail.com>
2025-12-21 16:21:44 -0500

    Account

--- src/accountcreation/SignIn.jsx
+++ src/accountcreation/SignIn.jsx
@@ -1,7 +1,25 @@
 import { useState } from 'react'
+import supabase  from '../config/supabaseClient.js'
+import { useNavigate } from 'react-router-dom';
 
 function SignIn() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
+  async function checkSignin() {
+    if (!username || !password) {
+      alert("Please fill in all required fields!");
+      return;
+    }
+    const response = await supabase.auth.signInWithPassword(username.trim(), password);
+    const error = response.error;
+    if (error) {
+      alert("Sign in failed.");
+      return;
+    }
+    navigate("/");
+  }
   return (
     <div className='w-full bg-blue-100 h-screen p-4 flex flex-col items-center justify-center font-outfit'>
       <div className='bg-gray-50 shadow-xl w-1/3 min-h-fit rounded-xl flex flex-col p-6 gap-6 border-blue-500 border-2'>
@@ -14,14 +32,16 @@ function SignIn() {
 
         <div className='w-full flex flex-col gap-4'>
           <input
-          type="text"
-          placeholder="Username"
-          className='p-3 border border-gray-300 rounded-lg focus:outline-none'
+            type="text"
+            placeholder="Username"
+            className='p-3 border border-gray-300 rounded-lg focus:outline-none'
+            onChange={(ev) => setUsername(ev.target.value)}
           />
           <input
             type="password"
             placeholder="Password"
             className='p-3 border border-gray-300 rounded-lg focus:outline-none'
+            onChange={(ev) => setPassword(ev.target.value)}
           />
         </div>
         
@@ -33,7 +53,9 @@ function SignIn() {
         </div>
 
         <div className='mt-auto'>
-          <button className='w-1/2 bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-300 flex justify-center mx-auto'>
+          <button
+            onClick={() => checkSignin()}
+            className='w-1/2 bg-blue-500 text-white p-3 rounded-lg hover:bg-blue-600 transition duration-300 flex justify-center mx-auto'>
             Sign In
           </button>
         </div>
--- src/accountcreation/SignUp.jsx
+++ src/accountcreation/SignUp.jsx
@@ -1,10 +1,39 @@
 import { useState } from 'react'
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
+import supabase  from '../config/supabaseClient.js'
 
 function SignUp() {
+    const [username, setUsername] = useState(null);
+    const [email, setEmail] = useState(null);
     const [classGrade, setClassGrade] = useState("Select Class Grade");
+    const [phoneNum, setPhoneNum] = useState(null);
+    const [password, setPassword] = useState(null);
 
+  async function submission(){
+    ev.preventDefault();
+    if (!username || !email || classGrade == "Select Class Grade" || !phoneNum || !password) {
+      alert("Please fill in all required fields!");
+      return;
+    }
+    const { data, error } = await supabase
+      .from("items")
+      .insert({
+        user_name: username,
+        e_mail: email,
+        class_grade: classGrade,
+        phone_num: phoneNum,
+        account_password: password
+      });
+
+    if (error) {
+      console.error("Insert error:", error);
+      alert("Failed to create account.");
+      return;
+    }
+
+    alert("Account created successfully!");
+  } 
   return (
     <div className='w-full min-h-screen bg-blue-100 p-4 flex flex-col items-center justify-center font-outfit'>
 
@@ -30,12 +59,14 @@ function SignUp() {
             type="text"
             placeholder="Username"
             className='p-3 bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400'
+            onChange={(ev) => setUsername(ev.target.value)}
           />
 
           <input
             type="email"
             placeholder="Email"
             className='p-3 bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400'
+            onChange={(ev) => setEmail(ev.target.value)}
           />
 
           {/* Grade Dropdown */}
@@ -45,10 +76,10 @@ function SignUp() {
               title={classGrade} 
               className="w-full"
             >
-              <Dropdown.Item as="button">9th</Dropdown.Item>
-              <Dropdown.Item as="button">10th</Dropdown.Item>
-              <Dropdown.Item as="button">11th</Dropdown.Item>
-              <Dropdown.Item as="button">12th</Dropdown.Item>
+              <Dropdown.Item as="button" onClick={() => setClassGrade("9th")} >9th</Dropdown.Item>
+              <Dropdown.Item as="button" onClick={() => setClassGrade("10th")}>10th</Dropdown.Item>
+              <Dropdown.Item as="button" onClick={() => setClassGrade("11th")}>11th</Dropdown.Item>
+              <Dropdown.Item as="button" onClick={() => setClassGrade("12th")}>12th</Dropdown.Item>
             </DropdownButton>
           </div>
 
@@ -56,18 +87,22 @@ function SignUp() {
             type="tel"
             placeholder="Phone Number"
             className='p-3 bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400'
+            onChange={(ev) => setPhoneNum(ev.target.value)}
           />
 
           <input
             type="password"
             placeholder="Password"
             className='p-3 bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400'
+            onChange={(ev) => setPassword(ev.target.value)}
           />
 
         </div>
 
         {/* Button */}
-        <button className='mt-4 w-2/3 bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition flex justify-center mx-auto shadow-md'>
+        <button 
+          onClick={() => submission()}
+          className='mt-4 w-2/3 bg-blue-600 text-white py-3 rounded-lg hover:bg-blue-700 transition flex justify-center mx-auto shadow-md'>
           Create Account
         </button>
 
